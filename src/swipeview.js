2e70a82158334c8647ab6bf6d10c276ac97ae4bd
Joe Bartlett <contact@jdbartlett.com>
2013-10-28 03:09:26 -0500

    Add left position for Ff transitions
    Firefox's transition support is detected, but not its transform
    support; result being that the transition is based on CSS position
    rather than translation.
    
    Obviously that needs to be corrected, but my immediate issue is
    that because no initial left position is set, there's no transition
    when `.next()` is called before manual swiping and consequently no
    transitionend event. That's a problem when using pagination like in
    the first gallery demo.

--- src/swipeview.js
+++ src/swipeview.js
@@ -200,7 +200,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
             div = document.createElement('div');
             div.className = 'swipeview-slider';
-            div.style.cssText = 'position:relative;top:0;height:100%;width:100%;' +
+            div.style.cssText = 'position:relative;top:0;left:0;height:100%;width:100%;' +
                 cssVendor + 'transition-duration:0;' +
                 cssVendor + 'transform:translateZ(0);' +
                 cssVendor + 'transition-timing-function:ease-out';
